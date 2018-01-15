df6352f7acf2e3bb6e154cfae86dea8ea6a18cb1
Jeremiah Hanson <hanson.jeremiah@comcast.net>
2018-01-15 16:42:28 -0700

    Created Systems and SysObj classes

---
+++ src/systems/Systems.java
@@ -0,0 +1,100 @@
+/*
+ * Class: Systems
+ * Author: Jeremiah Hanson
+ * ----------------------------------------------------------------------------
+ * Purpose: The purpose of this class is to create a hashmap that stores all of
+ * objects that represent the various systems and sysObjects on the robot, such
+ * as solenoids and speed-controllers 
+ * 
+ * Responsibilities: Responsible for maintaining the systems so that no class 
+ * should have to create their own system objects but instead ask this class 
+ * for those objects.
+ */
+
+package systems;
+
+import java.util.HashMap;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+
+public class Systems {
+	
+	private static Systems systems;
+	HashMap<SysObj, Object> sysObjects;
+	
+	/*
+	 * Constructor
+	 * Author: Jeremiah Hanson
+	 * -------------------------------------------------
+	 * Singlton constructor for the systems
+	 */
+	private Systems(){
+
+		sysObjects = new HashMap<SysObj, Object>();
+		
+//=======================================================================================
+// Motor Controllers
+//=======================================================================================
+
+		// Left Motor Controllers
+		sysObjects.put(SysObj.MotorController.LEFT_1, new WPI_TalonSRX(2));
+		sysObjects.put(SysObj.MotorController.LEFT_2, new WPI_TalonSRX(4));
+		sysObjects.put(SysObj.MotorController.LEFT_3, new WPI_TalonSRX(6));
+		
+		// Right Motor Controllers
+		sysObjects.put(SysObj.MotorController.RIGHT_1, new WPI_TalonSRX(1));
+		sysObjects.put(SysObj.MotorController.RIGHT_2, new WPI_TalonSRX(3));
+		sysObjects.put(SysObj.MotorController.RIGHT_3, new WPI_TalonSRX(5));
+		
+		// Arm Motor Controllers
+		sysObjects.put(SysObj.MotorController.COLLECTOR_ARM, new WPI_TalonSRX(0));
+		sysObjects.put(SysObj.MotorController.INTAKE_LEFT, new WPI_TalonSRX(7));
+		sysObjects.put(SysObj.MotorController.INTAKE_RIGHT, new WPI_TalonSRX(8));
+		
+		// Climber Motor Controller(s)
+		sysObjects.put(SysObj.MotorController.CLIMBER, new WPI_TalonSRX(9));
+
+//=======================================================================================
+// Sensors
+//=======================================================================================
+
+		// Encoders
+		sysObjects.put(SysObj.Sensors.CLIMB_ENCODER, new Encoder(8,9));
+		sysObjects.put(SysObj.Sensors.ARM_ENCODER, new Encoder(4,5));
+		sysObjects.put(SysObj.Sensors.LEFT_ENCODER, new Encoder(2,3));
+		sysObjects.put(SysObj.Sensors.RIGHT_ENCODER, new Encoder(1,0));
+		
+		// Joysticks
+		sysObjects.put(SysObj.Sensors.DRIVER_STICK, new Joystick(0));
+		sysObjects.put(SysObj.Sensors.OPERATOR_STICK, new Joystick(1));
+		((Joystick)sysObjects.get(SysObj.Sensors.OPERATOR_STICK)).setRumble(RumbleType.kLeftRumble, 0);
+		((Joystick)sysObjects.get(SysObj.Sensors.OPERATOR_STICK)).setRumble(RumbleType.kRightRumble, 0);
+		
+		// Other Sensors
+		sysObjects.put(SysObj.Sensors.PDP, new PowerDistributionPanel(0));
+		
+		
+		
+	}
+	
+	/*
+	 * getInstance
+	 * Author: Jeremiah Hanson
+	 * --------------------------------------------------
+	 * Purpose: gets the single instance of Systems. Creates
+	 * 	Systems if one does not exist
+	 * Returns: a Systems object
+	 */
+	public Systems getInstance() {
+		if (systems == null) {
+			systems = new Systems();
+		}
+		return systems;
+	}
+
+}
