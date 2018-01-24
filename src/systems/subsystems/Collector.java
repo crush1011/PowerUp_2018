02883fec3ba87a922ada67ef80ce4e0cc7a5ded8
Jeremiah Hanson <hanson.jeremiah@comcast.net>
2018-01-23 17:29:24 -0700

    Added collector class with constructor

---
+++ src/systems/subsystems/Collector.java
@@ -0,0 +1,56 @@
+/*
+ * Collector.java
+ * Author: Nitesh Puri
+ * Collaborator: Jeremiah Hanson
+ * -----------------------------------------------------
+ * This class controls the motors on the collector
+ */
+
+
+package systems.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Spark;
+import systems.Subsystem;
+import systems.Systems;
+
+public class Collector implements Subsystem {
+	
+	private WPI_TalonSRX collectorArm; 
+	Spark intakeLeft, intakeRight;
+	private static Systems systems;
+	
+	/*
+	 * Constructor
+	 * Author: Nitesh Puri
+	 * ----------------------------------------
+	 * constructor
+	 */
+	
+	public Collector(WPI_TalonSRX collectorArm, 
+			Spark intakeLeft, 
+			Spark intakeRight) {
+		this.collectorArm = collectorArm;
+		this.intakeLeft = intakeLeft;
+		this.intakeRight = intakeRight;
+		intakeLeft.setInverted(true);
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see systems.Subsystem#update()
+	 */
+	@Override
+	public void update() {
+		//intakeLeft.set(systems.);
+		
+	}
+
+	@Override
+	public void toSmartDashboard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
