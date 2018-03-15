d6d33aa377cb5f03e68f0199f7721c79e4459f10
Jeremiah Hanson <hanson.jeremiah@comcast.net>
2018-03-14 19:03:46 -0700

    Added Circle

---
+++ src/autonomous/AutonCircle.java
@@ -0,0 +1,162 @@
+package autonomous;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import systems.Resources;
+import systems.SysObj;
+import systems.Systems;
+import systems.subsystems.DriveTrain;
+import systems.subsystems.NavX;
+
+public class AutonCircle implements Runnable {
+
+	double angle, topSpeed, distance, radius;
+	double rotateOutput;
+	double loopCount, pastCount;
+
+	boolean right;
+	
+	final double acceleration = 300;
+	final double kA = (1 / acceleration) * 0;
+	final double wheelDistance = 25;
+
+	DriveTrain drive;
+
+	NavX navx;
+
+	private static final double P = 0.02;
+
+	public AutonCircle(DriveTrain driveTrain, NavX navx, double radius, double topSpeed, double angle) {
+
+		this.angle = angle;
+		this.radius = radius;
+		this.topSpeed = topSpeed;
+		this.distance = (angle/360) * 2 * Math.PI * radius;  
+
+		this.drive = driveTrain;
+		this.navx = navx;
+	}
+
+	double delT = 50;
+
+	public void run() {
+		boolean stop = false;
+		// 300 is accel
+
+		int counter =0;
+		boolean deAccelerate = false;
+		boolean deAccelerateStarted = false;
+		double initialPos = Systems.getInstance().getAverageDriveEncoderDistance();
+
+		double distanceTravelled = 0;
+		double pastDistanceTravelled = 0;
+		
+		double leftMod = 1;
+		double rightMod = 1;
+		
+		if(right) {
+			rightMod = radius / (radius + wheelDistance); 
+		}
+		if(!right) {
+			leftMod = radius / (radius + wheelDistance);
+		}
+
+		boolean backwards = distance < 0;
+
+		double currentVelocity = 0;
+		double lastVelocity = 0;
+
+		long startTimeLine = System.currentTimeMillis();
+		while (!stop && DriverStation.getInstance().isAutonomous() && DriverStation.getInstance().isEnabled()) {
+			long startTime = System.currentTimeMillis();
+
+			double timeToStop = Math.abs(currentVelocity / acceleration);
+
+			double distanceNeededToStop = (Math.abs(currentVelocity)) * timeToStop;
+
+			if(right) {
+				distanceTravelled = Math.abs(Systems.getInstance().getEncoderDistance(SysObj.Sensors.LEFT_ENCODER) - initialPos);
+			}
+			if(!right) {
+				distanceTravelled = Math.abs(Systems.getInstance().getEncoderDistance(SysObj.Sensors.RIGHT_ENCODER) - initialPos);
+			}
+			
+			// remainingdistance
+			if (Math.abs(distance) - distanceTravelled <= distanceNeededToStop) {
+				deAccelerate = true;
+				deAccelerateStarted=true;
+			} else if ((Math.abs(distance) > distanceTravelled)) {
+				//deAccelerate = false;
+			}
+
+			double currentAcceleration;
+			if (deAccelerate ? !backwards : backwards) {
+				currentVelocity -= (delT / 1000) * acceleration;
+				currentAcceleration = -acceleration;
+			} else {
+				currentVelocity += (delT / 1000) * acceleration;
+				currentAcceleration = acceleration;
+			}
+			if (Math.abs(currentVelocity) - topSpeed >= 0) {
+				currentAcceleration = 0;
+			}
+			currentVelocity = Math.max(Math.min(topSpeed, currentVelocity), -topSpeed);
+			System.out.println(
+					"CurrentTime:" + (System.currentTimeMillis() - startTimeLine) + "    currentV:" + currentVelocity
+							+ "    CURRENTP" + distanceTravelled + "    DistanceToSTOP" + distanceNeededToStop);
+			System.out.println("DEACC" + deAccelerate);
+
+			double currentError = Resources.getAngleError(angle, navx.getCurrentAngle());
+			
+
+			double rotateOutput = currentError * P;
+			
+			System.out.println("CurrentError: " + currentError  + "   rotateOutput: " + rotateOutput + "   Angle: " + navx.getCurrentAngle());
+			
+			pastCount = loopCount;
+			double actualVelocity = currentVelocity / 150;
+			drive.tankDrive(leftMod * (actualVelocity + (kA * currentAcceleration)), rightMod * (actualVelocity + (kA * currentAcceleration)));
+			if (backwards) {
+				if (currentVelocity >= 0 && lastVelocity < 0) {
+					stop = true;
+				}
+			} else {
+				if (currentVelocity <= 0 && lastVelocity > 0) {
+					stop = true;
+				}
+			}
+			if((Math.abs(distanceTravelled - pastDistanceTravelled) < 0.1 )){
+				counter++;
+			}
+			if(counter>20){
+				stop = true;
+			}
+			lastVelocity = currentVelocity;
+			try {
+				long sleepTime = ((long) delT - (System.currentTimeMillis() - startTime));
+				System.out.println("SLEEP" + sleepTime);
+				Thread.sleep(sleepTime);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		long startTime = System.currentTimeMillis();
+		/*while (System.currentTimeMillis() - startTime < 300) {
+			drive.drive(backwards ? 0.5 : -0.5, 0);
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}*/
+		drive.drive(0, 0);
+
+	}
+
+}
